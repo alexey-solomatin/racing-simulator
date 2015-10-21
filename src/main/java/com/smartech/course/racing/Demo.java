8f32da22499292704a2d0fe25eceae31b65dc29f
Alexey Solomatin <alexey.solomatin@gmail.com>
2015-10-21 16:44:17 +0300

    Added the demo class. Corrected the project settings.

---
+++ src/main/java/com/smartech/course/racing/Demo.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.smartech.course.racing;
+
+/**
+ * Simple demonstration of a Java application
+ * @author Alexey Solomatin
+ *
+ */
+public class Demo {
+
+	/**
+	 * Entry point to the application
+	 * @param args command line arguments
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello! This is Racing Simulator!");
+	}
+
+}
